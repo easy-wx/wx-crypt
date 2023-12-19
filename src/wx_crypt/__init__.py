f98af48ee74d40152f973ad5dc440d38524a9680
panzhongxian <panzhongxian0532@gmail.com>
2023-12-19 12:43:08 +0800

    Init

---
+++ src/wx_crypt/__init__.py
@@ -0,0 +1,8 @@
+from wx_crypt.ierror import *
+from wx_crypt.WXBizMsgCrypt3 import WXBizMsgCrypt
+# 平台相关
+from wx_crypt.WXBizMsgCrypt3 import WxChannel_Wecom, WxChannel_Mp
+
+__author__ = "Pan Zhongxian(panzhongxian0532@gmail.com)"
+
+__all__ = ["WXBizMsgCrypt", "WxChannel_Wecom", "WxChannel_Mp", "ierror"]
---
+++ src/wx_crypt/channel.py
@@ -0,0 +1,2 @@
+WxChannel_Wecom = 1  # 企业微信
+WxChannel_Mp = 2  # 公众号
